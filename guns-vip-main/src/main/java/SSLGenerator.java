a5d54aaedde249b8c4497b01c5681a6f97008a2a
dunn <dunn.work.gmail>
2021-04-05 22:09:43 +0800

    修改ssl申请证书

--- guns-vip-main/src/main/java/SSLGenerator.java
+++ guns-vip-main/src/main/java/SSLGenerator.java
@@ -41,7 +41,7 @@ public class SSLGenerator {
     private static final int BATCH = 300;//一波最多300条，不要改
 
     public static void main(String[] args) {
-        genSH(SAVE_PATH + "\\sh"); //生成脚本
+//        genSH(SAVE_PATH + "\\sh"); //生成脚本
 
 
 //        genNginxCfgJoin(SAVE_PATH + "\\cfg"); //生成配置文件
